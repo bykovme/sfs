44d40e94dbaae6e39a4f7e8132c5df71091289a0
Alex Bykov <alex@bykov.me>
2020-07-13 00:52:55 +0300

    api check

--- internal/sfsapp/api_download.go
+++ internal/sfsapp/api_download.go
@@ -1,7 +1,12 @@
 package sfsapp
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func (app *App) handleDownload(w http.ResponseWriter, r *http.Request) {
-
+	//w.Header().Set("Content-Disposition", "attachment; filename="+strconv.Quote(filename))
+	//w.Header().Set("Content-Type", "application/octet-stream")
+	//http.ServeFile(w, r, filePath)
+	// http.ServeFile(w,r,fileData)
 }
